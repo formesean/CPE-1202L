7cc87bb943ded048ee1c6d98cac259da2e221f53
Sean Aguilar <seanaguilar698@gmail.com>
2023-02-04 00:39:15 +0800

    learning stack DS

--- Practice/Stack/Stack_02.c
+++ Practice/Stack/Stack_02.c
@@ -9,9 +9,50 @@ struct node
     struct node *next;
 };
 
-struct node *head = NULL;
+struct node *top = NULL;
+
+void push(int data)
+{
+    struct node *newNode = (struct node *)malloc(sizeof(struct node));
+    newNode->data = data;
+    newNode->next = top;
+    top = newNode;
+}
+
+void pop()
+{
+    struct node *temp;
+
+    if (top == NULL) // stack is empty
+        return;
+
+    temp = top;
+    top = top->next;
+    free(temp);
+}
+
+void display()
+{
+    struct node *current = top;
+
+    printf("Stack: ");
+    while (current != NULL)
+    {
+        printf("%d ", current->data);
+        current = current->next;
+    }
+    printf("\n");
+}
 
 int main()
 {
+    push(3);
+    push(6);
+    push(9);
+    display();
+
+    pop();
+    display();
+
     return 0;
 }

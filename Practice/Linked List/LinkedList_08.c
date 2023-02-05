04924cced4767f892f9cca2f36df16ddfb93cb5e
Sean Aguilar <seanaguilar698@gmail.com>
2023-02-05 18:37:38 +0800

    Circular Linked List

---
+++ Practice/Linked List/LinkedList_08.c
@@ -0,0 +1,69 @@
+// Cirular Linked List
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+struct node *head = NULL;
+
+void insert(int data)
+{
+    struct node *newNode = (struct node *)malloc(sizeof(struct node));
+    newNode->data = data;
+
+    if (head == NULL)
+    {
+        head = newNode;
+        newNode->next = head;
+        return;
+    }
+
+    struct node *temp = head;
+    while (temp->next != head)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    newNode->next = head;
+}
+
+void display()
+{
+    struct node *current = head;
+    printf("Linked List: ");
+    do
+    {
+        printf("%d ", current->data);
+        current = current->next;
+    } while (current != head);
+    printf("\n");
+}
+
+int main()
+{
+    int data, num;
+
+    // printf("Enter number of nodes: ");
+    // scanf("%d", &num);
+
+    // for (int i = 0; i < num; i++)
+    // {
+    //     printf("Enter number: ");
+    //     scanf("%d", &data);
+    //     insert(data);
+    // }
+
+    insert(3);
+    insert(6);
+    insert(9);
+    insert(12);
+
+    display();
+
+    return 0;
+}
